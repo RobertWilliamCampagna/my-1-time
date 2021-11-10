83c92291d7403d4b0fd412f44a5f03be5d71755b
Robert Campagna <rwcnyla@gmail.com>
2021-11-10 14:34:51 -0800

    first commit, made welcome page

---
+++ my-1-time/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Welcome></Welcome>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-1-time/src/components/Welcome/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome (){
+    return (
+        <section>
+            <h1 id="welcome">Welcome to My-1st-Time</h1>
+        </section>
+    );
+}
+
+export default Welcome;
